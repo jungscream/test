367f78ba62a70308395e9c8f45c809ef4b5a047f
J3rry33 <lancia01@naver.com>
2025-05-10 13:59:34 +0900

    test

--- test.js
+++ test.js
@@ -2,8 +2,27 @@ const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3();
 
-s3.putObject({
-    Body: 'hello world',
-    Bucket: 'jungscream-test',
-    Key: 'my-file.txt',
-}).promise();
+// s3.putObject({
+//     Body: 'hello world',
+//     Bucket: 'jungscream-test',
+//     Key: 'my-file.txt',
+// }).promise();
+
+
+
+  const key = `test.txt`;
+
+(async () => {
+    const command = new PutObjectCommand({
+        Bucket: "jungscream-test",
+        Key: key,
+        Body: "hello world"
+    });
+    try {
+        await s3.send(command);
+        console.log(`S3 저장 완료: ${key}`);
+    } catch (err) {
+        console.error(`S3 저장 실패 (${label}):`, err.message);
+    }
+})();
+
