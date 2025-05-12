fd333b15b5a1573f95ba2935765b800b56f0eb62
J3rry33 <lancia01@naver.com>
2025-05-12 20:38:20 +0900

    test

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/api/start', async (req, res) => {
+oauthApp.get('/api/start', async (req, res) => {
   const token = loadAccessToken();
   try {
     const params = new URLSearchParams({
